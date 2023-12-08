96af70fc8661214312f39abfe918c51a71dfc9e9
Generic Bliss account for Control Software <blissadm@lid29mono.esrf.fr>
2023-12-08 14:04:31 +0100

    [ID29 Task Dialog] - json-schema-form API change

--- mxcubeweb/core/components/queue.py
+++ mxcubeweb/core/components/queue.py
@@ -1624,6 +1624,7 @@ class Queue(ComponentBase):
         """
         sample_model, sample_entry = self.get_entry(node_id)
         model, entry = self._create_queue_entry(task, task_name)
+        model.set_origin(ORIGIN_MX3)
 
         acq = model.acquisitions[0]
         params = task["parameters"]
@@ -1658,7 +1659,8 @@ class Queue(ComponentBase):
             )
 
         full_path, process_path = HWR.beamline.session.get_full_path(
-            params.get("subdir", ""), task_name
+            os.path.join(params.get("subdir", ""), params.get("experiment_name", "")),
+            task_name
         )
         acq.path_template.directory = full_path
         acq.path_template.process_directory = process_path
@@ -2096,6 +2098,7 @@ class Queue(ComponentBase):
         return self.app.AUTO_MOUNT_SAMPLE
 
     def get_task_progress(self, node, pdata):
+        return 0
         progress = 0
 
         if node.is_executed():
