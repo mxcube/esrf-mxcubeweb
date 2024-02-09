a0c40137f8f81d64b31b07eb2fde0515ea87e054
Marcus Oskarsson <oscarsso@esrf.fr>
2024-02-09 16:25:36 +0100

    [Task dialog] -  Added get_resolution_limits_for_energy that retrieves the resolution limits for a given energy

--- mxcubeweb/core/adapter/energy_adapter.py
+++ mxcubeweb/core/adapter/energy_adapter.py
@@ -1,5 +1,6 @@
 from mxcubeweb.core.adapter.actuator_adapter import ActuatorAdapter
 from mxcubeweb.core.adapter.wavelength_adapter import WavelengthAdapter
+from mxcubecore import HardwareRepository as HWR
 
 
 class EnergyAdapter(ActuatorAdapter):
@@ -8,6 +9,8 @@ class EnergyAdapter(ActuatorAdapter):
     information on longer running processes.
     """
 
+    METHODS = ["get_resolution_limits_for_energy"]
+
     def __init__(self, *args, **kwargs):
         """
         Args:
@@ -15,4 +18,7 @@ class EnergyAdapter(ActuatorAdapter):
         """
         super(EnergyAdapter, self).__init__(*args, **kwargs)
         self._add_adapter("wavelength", self._ho, WavelengthAdapter)
-        self._type = "MOTOR"
+        self._type = "ENERGY"
+
+    def get_resolution_limits_for_energy(self, energy: float) -> tuple:
+        return HWR.beamline.resolution.get_limits(self._ho.calculate_energy(energy))
