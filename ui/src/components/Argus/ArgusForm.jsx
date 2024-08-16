16e5b50ceb9b653607e3ce4cf8571aa8166f9379
walesch-yan <yan.walesch@esrf.fr>
2024-08-16 17:32:54 +0200

    Added Argus Proccess control panel for sending commands

--- ui/src/components/Argus/ArgusForm.jsx
+++ ui/src/components/Argus/ArgusForm.jsx
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 
+import ArgusProcessControl from './ArgusProcessControl';
 import styles from './Argus.module.css';
 
-function capitalize(string) {
+export function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export function ArgusForm(props) {
   const { argus, handleHide, sendExecuteCommand, show } = props;
+
+  const [startState, setStartState] = useState({
+    show: false,
+    title: 'Default',
+    initArgs: [],
+  });
+  const [sendCommandState, setSendCommandState] = useState({
+    show: false,
+    title: 'Default',
+    commands: {},
+  });
+
+  const showStart = (title, initArgs) => {
+    setStartState((prevState) => ({
+      ...prevState,
+      show: true,
+      title,
+      initArgs,
+    }));
+  };
+
+  const showSendCommand = (title, commands) => {
+    setSendCommandState((prevState) => ({
+      ...prevState,
+      show: true,
+      title,
+      commands,
+    }));
+  };
+
+  const hideChildren = () => {
+    setStartState((prevState) => ({
+      ...prevState,
+      show: false,
+    }));
+    setSendCommandState((prevState) => ({
+      ...prevState,
+      show: false,
+    }));
+  };
 
   const handleCancel = () => {
     handleHide();
@@ -16,12 +57,19 @@ export function ArgusForm(props) {
 
   const { processes_info, last_response } = argus.attributes;
   const { status, error_message } = last_response;
-  const capitalizedStatus = `${status ? capitalize(status) : ''}! `;
+  const capitalizedStatus = status ? `${capitalize(status)} !` : '';
 
   const runningButtons = (key) => {
     return (
       <div className={styles.processButtons}>
-        <Button variant="primary">Send Command</Button>
+        <Button
+          variant="primary"
+          onClick={() =>
+            showSendCommand(key, processes_info.running[key].commands)
+          }
+        >
+          Send Command
+        </Button>
         <Button
           variant="danger"
           onClick={() =>
@@ -35,45 +83,70 @@ export function ArgusForm(props) {
   };
 
   return (
-    <Modal id="ArgusModal" show={show} onHide={handleCancel}>
-      <Modal.Header closeButton>
-        <Modal.Title>Argus Processes</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {Object.keys(processes_info.running).map((key) => (
-          <div className={styles.processContainer} key={`${key}-process`}>
-            <span>
-              {key}
-              {` (${processes_info.running[key].type})`}
-            </span>
-            {key === 'Error' ? <div /> : runningButtons(key)}
-          </div>
-        ))}
-        <hr />
-        <div className={styles.typesContainer}>
-          {Object.keys(processes_info.available).map((type) => (
-            <Button
-              className={styles.typeButton}
-              variant="outlined"
-              key={`${type}-typeButton`}
-            >
-              {capitalize(type)}
-            </Button>
+    <div>
+      <ArgusProcessControl
+        state={startState}
+        type="start"
+        hide={hideChildren}
+        sendExecuteCommand={sendExecuteCommand}
+      />
+      <ArgusProcessControl
+        state={sendCommandState}
+        type="sendCommand"
+        hide={hideChildren}
+        sendExecuteCommand={sendExecuteCommand}
+      />
+      <Modal
+        id="ArgusModal"
+        show={show && !startState.show && !sendCommandState.show}
+        onHide={handleCancel}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Argus Processes</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {Object.keys(processes_info.running).map((key) => (
+            <div className={styles.processContainer} key={`${key}-process`}>
+              <span>
+                {key}
+                {` (${processes_info.running[key].type})`}
+              </span>
+              {key === 'Error' ? <div /> : runningButtons(key)}
+            </div>
           ))}
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        {last_response ? (
-          <div>
-            <span style={{ color: status === 'success' ? 'green' : 'red' }}>
-              {capitalizedStatus}
-            </span>
-            <span>{error_message}</span>
-          </div>
-        ) : (
-          <div />
-        )}
-      </Modal.Footer>
-    </Modal>
+          <hr />
+          {Object.keys(processes_info.available).length === 0 ? (
+            <span>No available processes</span>
+          ) : (
+            <div className={styles.typesContainer}>
+              {Object.keys(processes_info.available).map((type) => (
+                <Button
+                  className={styles.typeButton}
+                  variant="outlined"
+                  key={`${type}-typeButton`}
+                  onClick={() =>
+                    showStart(type, processes_info.available[type].args)
+                  }
+                >
+                  {capitalize(type)}
+                </Button>
+              ))}
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          {last_response ? (
+            <div>
+              <span style={{ color: status === 'success' ? 'green' : 'red' }}>
+                {capitalizedStatus}
+              </span>
+              <span>{error_message}</span>
+            </div>
+          ) : (
+            <div />
+          )}
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
---
+++ ui/src/components/Argus/ArgusProcessControl.jsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from 'react';
+import { Modal, Form, Button } from 'react-bootstrap';
+import { capitalize } from './ArgusForm';
+
+import styles from './Argus.module.css';
+
+export default function ArgusProcessControl(props) {
+  const { state, type, hide, sendExecuteCommand } = props;
+  const { show, title, initArgs, commands } = state;
+  const [formData, setFormData] = useState({});
+  const [availableCommands, setAvailableCommands] = useState({
+    selectedCommand: '',
+  });
+
+  const setCommandData = (args) => {
+    const current_command_data = { wait_time: '5' };
+    if (args) {
+      args.forEach((arg) => {
+        current_command_data[arg] = '';
+      });
+    }
+    setFormData(current_command_data);
+  };
+
+  useEffect(() => {
+    if (type === 'start') {
+      const data = { name: '' };
+      if (initArgs) {
+        initArgs.forEach((arg) => {
+          data[arg] = '';
+        });
+      }
+      setFormData(data);
+    } else if (type === 'sendCommand') {
+      if (!commands) {
+        setFormData({});
+        setAvailableCommands({ selectedCommand: '' });
+        return;
+      }
+
+      const command_data = Object.values(commands).reduce(
+        (acc, { command, args }) => {
+          if (!acc[command]) {
+            acc[command] = [];
+          }
+          if (args) {
+            acc[command].push(...args);
+          }
+          return acc;
+        },
+        {},
+      );
+
+      const keys = Object.keys(command_data);
+      if (keys.length > 0) {
+        setCommandData(command_data[keys[0]]);
+        setAvailableCommands({ selectedCommand: keys[0], ...command_data });
+      } else {
+        setAvailableCommands({ selectedCommand: '' });
+      }
+    }
+  }, [type, initArgs, commands]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleCommandSelection = (e) => {
+    setAvailableCommands((prevstate) => ({
+      ...prevstate,
+      selectedCommand: e.target.value,
+    }));
+    setCommandData(availableCommands[e.target.value]);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, wait_time, ...args } = formData;
+
+    if (type === 'start') {
+      sendExecuteCommand('argus', 'start_process', {
+        name,
+        type: title,
+        args: Object.values(args),
+      });
+    } else if (type === 'sendCommand') {
+      const { selectedCommand } = availableCommands;
+      sendExecuteCommand('argus', 'manage_process', {
+        name: title,
+        command: selectedCommand,
+        wait_time: Number.parseInt(wait_time),
+        args: Object.values(args),
+      });
+    }
+
+    hide();
+  };
+
+  return (
+    <Modal id={`${type}-${title}`} show={show} onHide={hide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{`${capitalize(type)}: ${capitalize(title)}`}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {type === 'sendCommand' ? (
+          <Form.Select onChange={handleCommandSelection}>
+            {Object.keys(availableCommands).map((key) =>
+              key === 'selectedCommand' ? null : (
+                <option key={`${key}-option`}>{key}</option>
+              ),
+            )}
+          </Form.Select>
+        ) : (
+          <div />
+        )}
+        <Form onSubmit={onSubmit}>
+          <div className={styles.commandContainer}>
+            {Object.keys(formData).map((key) => (
+              <Form.Label key={`${key}-label`}>
+                {capitalize(key)}:
+                <Form.Control
+                  label={capitalize(key)}
+                  name={key}
+                  type="text"
+                  value={formData[key]}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Label>
+            ))}
+          </div>
+          <div className={styles.submitButtonContainer}>
+            <Button type="submit">Submit</Button>
+          </div>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+}
