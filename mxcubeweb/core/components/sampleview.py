6cd9d6cbb7bd15ef3f45b4f1a64e299f5c334080
Marcus Oskarsson <oscarsso@esrf.fr>
2024-11-08 09:09:03 +0100

    [3-Click centring] - Temporary fix unitl source of problem found, it seems like fast clicking can lead to negative click count

--- mxcubeweb/core/components/sampleview.py
+++ mxcubeweb/core/components/sampleview.py
@@ -274,6 +274,7 @@ class SampleView(ComponentBase):
 
     def update_shapes(self, shapes):
         updated_shapes = []
+
         for s in shapes:
             shape_data = from_camel(s)
             pos = []
@@ -415,10 +416,13 @@ class SampleView(ComponentBase):
 
     def centring_handle_click(self, x, y):
         if HWR.beamline.diffractometer.current_centring_procedure:
-            HWR.beamline.diffractometer.imageClicked(x, y, x, y)
+            HWR.beamline.diffractometer.image_clicked(x, y, x, y)
             self.centring_click()
         else:
-            if not self.centring_clicks_left():
+            # Number of centring clicks can still be negative if the user
+            # clicks very frequently on the image, added comparsion with lesser
+            # then 0 to be able to recover from this scenario.
+            if self.centring_clicks_left() <= 0:
                 self.centring_reset_click_count()
                 HWR.beamline.diffractometer.cancel_centring_method()
 
