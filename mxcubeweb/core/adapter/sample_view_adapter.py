9325c7dea9ad8a81600ee7ad0859a9ef478b491e
Marcus Oskarsson <oscarsso@esrf.fr>
2026-04-27 14:07:09 +0200

    Using cancel instead of reject centring so that no point is saved when centring is canceled

--- mxcubeweb/core/adapter/sample_view_adapter.py
+++ mxcubeweb/core/adapter/sample_view_adapter.py
@@ -464,7 +464,7 @@ class SampleViewAdapter(AdapterBase):
                 logging.getLogger("user_level_log").info(
                     "Aborting current centring ..."
                 )
-                self._ho.reject_centring()
+                self._ho.cancel_centring()
                 self._click_in_progress = False
             nb_clicks = HWR.beamline.config.click_centring_num_clicks
             msg = f"Centring using {nb_clicks}-click centring"
