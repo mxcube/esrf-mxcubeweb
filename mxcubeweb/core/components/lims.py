c386d73cde4e5d1113edd8be75ab47ba6debf838
Marcus Oskarsson <oscarsso@esrf.fr>
2026-06-02 10:35:51 +0200

    Fixed fullname for non SSO based logins

--- mxcubeweb/core/components/lims.py
+++ mxcubeweb/core/components/lims.py
@@ -93,7 +93,7 @@ class Lims(ComponentBase):
         :param proposal_number: Proposal number
         """
         # proposal_number is the session identifier
-        self.select_session(proposal_number)
+        self.select_session(proposal_number, current_user.username)
         self.app.usermanager.update_active_users()
         return {}
 
@@ -307,7 +307,7 @@ class Lims(ComponentBase):
     def allow_session(self, session):
         HWR.beamline.lims.allow_session(session)
 
-    def select_session(self, session_id: str) -> bool:
+    def select_session(self, session_id: str, username) -> bool:
         """Select session.
 
         Params:
@@ -317,8 +317,8 @@ class Lims(ComponentBase):
         """
         # Selecting the active session in the LIMS object
         try:
-            session = HWR.beamline.lims.set_active_session_by_id(session_id, current_user.username)
-            logging.getLogger("MX3.HWR").debug("user %s select_session session_id=%s" % (current_user.username, session_id))
+            session = HWR.beamline.lims.set_active_session_by_id(session_id, username)
+            logging.getLogger("MX3.HWR").debug("user %s select_session session_id=%s" % (username, session_id))
 
         except Exception as exc:
             logging.getLogger("MX3.HWR").exception(
